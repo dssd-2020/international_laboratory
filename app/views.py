b91b9f6b956388793294dd08ea0a9623030ece36
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-11-18 23:26:14 -0300

    Modificación de ActivityView

--- app/views.py
+++ app/views.py
@@ -1,39 +1,40 @@
+import requests
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
-import requests
-import json
 
-from .forms import ActivityForm, ProtocolForm, ProjectForm
+from .forms import ProtocolForm, ProjectForm
+from .models import Activity
 
 
 def createCase():
     loginFromBonita()
     headers = {
-            'X-Bonita-API-Token': requests.COOKIES['X-Bonita-API-Token'],
-            'JSESSIONID': requests.COOKIES['JSESSIONID'],
-            'csrftoken': requests.COOKIES['csrftoken'],
-            'Content-Type': 'application/json'
-            }
+        "X-Bonita-API-Token": requests.COOKIES["X-Bonita-API-Token"],
+        "JSESSIONID": requests.COOKIES["JSESSIONID"],
+        "csrftoken": requests.COOKIES["csrftoken"],
+        "Content-Type": "application/json"
+    }
 
-    response = requests.post('http://localhost:8080/bonita/API/bpm/case?f=processDefinitionId=40004', headers=headers)
+    response = requests.post("http://localhost:8080/bonita/API/bpm/case?f=processDefinitionId=40004", headers=headers)
     # response = requests.get(
-    #     'http://localhost:8080/bonita/API/bpm/process?f=name=AprobacionDeMedicamentos&p=0&c=10')
-        # , headers=headers)
-    print('case', response)
+    #     "http://localhost:8080/bonita/API/bpm/process?f=name=AprobacionDeMedicamentos&p=0&c=10")
+    # , headers=headers)
+    print("case", response)
     return response
 
 
 def getProcessId(request):
     request = loginFromBonita(request)
-    url = 'http://localhost:8080/bonita/API/bpm/process?c=100&p=0'
-    # url = 'http://localhost:8080/cookies'
+    url = "http://localhost:8080/bonita/API/bpm/process?c=100&p=0"
+    # url = "http://localhost:8080/cookies"
     headers = {
-            'X-Bonita-API-Token': request.COOKIES['X-Bonita-API-Token'],
-            # 'Content-Type': 'application/json',
-            'Cookie': request.COOKIES['JSESSIONID'],
-            # 'cache-control': 'no-cache',
-            }
+        "X-Bonita-API-Token": request.COOKIES["X-Bonita-API-Token"],
+        # "Content-Type": "application/json",
+        "Cookie": request.COOKIES["JSESSIONID"],
+        # "cache-control": "no-cache",
+    }
     # response = requests.post(url, headers=headers)
     response = requests.get(url, cookies=request.COOKIES)
     print(response)
@@ -41,79 +42,83 @@ def getProcessId(request):
 
 
 def getUsersFromBonita():
-    headers = {'X-Bonita-API-Token': loginFromBonita()}
+    headers = {"X-Bonita-API-Token": loginFromBonita()}
     response = requests.get(
-        'http://localhost:8080/bonita/API/identity/group/Grupo1')
-    # print(request.session['taskId'])
+        "http://localhost:8080/bonita/API/identity/group/Grupo1")
+    # print(request.session["taskId"])
     return response
 
 
 def loginFromBonita(request):
-    url = 'http://localhost:8080/bonita/loginservice'
-    data = {'username': 'walter.bates',
-            'password': 'bpm',
-            'redirect': 'false',
-            'redirectURL': ''}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    url = "http://localhost:8080/bonita/loginservice"
+    data = {"username": "walter.bates",
+            "password": "bpm",
+            "redirect": "false",
+            "redirectURL": ""}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(url, data=data, headers=headers)
 
     if response.status_code == 200:
-        request.session['X-Bonita-API-Token'] = request.COOKIES['X-Bonita-API-Token']
-        request.session['JSESSIONID'] = request.COOKIES['JSESSIONID']
-        # requests.session['csrftoken'] = request.COOKIES['csrftoken']
+        request.session["X-Bonita-API-Token"] = request.COOKIES["X-Bonita-API-Token"]
+        request.session["JSESSIONID"] = request.COOKIES["JSESSIONID"]
+        # requests.session["csrftoken"] = request.COOKIES["csrftoken"]
 
     return request
 
+
 class ActivityView(View):
-    form_class = ActivityForm
-    initial = {'key': '1'}
-    template_name = 'create_activity.html'
+    template_name = "create_activity.html"
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            activity = form.save()
-            return HttpResponseRedirect('/success/')
-
-        return render(request, self.template_name, {'form': form})
+        error = True
+        if "name" in request.POST:
+            try:
+                Activity.objects.create(
+                    name=request.POST.get("name")
+                )
+                error = False
+            except ():
+                pass
+        return JsonResponse({
+            "error": error
+        })
 
 
 class ProtocolView(View):
     form_class = ProtocolForm
-    initial = {'key': '1'}
-    template_name = 'create_protocol.html'
+    initial = {"key": "1"}
+    template_name = "create_protocol.html"
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             protocol = form.save()
-            return HttpResponseRedirect('/success/')
+            return HttpResponseRedirect("/success/")
 
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
 
 
 class ProjectView(View):
     form_class = ProjectForm
-    initial = {'key': '1'}
-    template_name = 'create_project.html'
+    initial = {"key": "1"}
+    template_name = "create_project.html"
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
         users = getProcessId(request)
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             activity = form.save()
-            return HttpResponseRedirect('/success/')
+            return HttpResponseRedirect("/success/")
 
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
