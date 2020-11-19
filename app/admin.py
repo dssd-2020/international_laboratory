197537b3b40ec9055cdc60fc326643513dfc1815
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-11-19 02:04:40 -0300

    Registro de modelos en admin

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Activity, Protocol, ActivityProtocol, Project
+from .models import Activity, Protocol, ActivityProtocol, Project, ProtocolProject
 
 admin.site.register(Activity)
 admin.site.register(Protocol)
 admin.site.register(ActivityProtocol)
 admin.site.register(Project)
+admin.site.register(ProtocolProject)
