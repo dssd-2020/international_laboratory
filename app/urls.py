4a5d5ff4581a00f6b8a6fcf919439b0fd5935ccb
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-11-18 23:26:27 -0300

    Agregado de name en paths

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from app.views import ProtocolView, ActivityView, ProjectView
 
 urlpatterns = [
-    path('protocolo', ProtocolView.as_view()),
-    path('actividad', ActivityView.as_view()),
-    path('proyecto', ProjectView.as_view()),
+    path("actividad", ActivityView.as_view(), name="actividad"),
+    path("protocolo", ProtocolView.as_view(), name="protocolo"),
+    path("proyecto", ProjectView.as_view(), name="proyecto"),
 ]
