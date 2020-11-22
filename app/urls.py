f5df7d00ffe9af96f82d18ccbe5f693965be53c1
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-11-22 18:42:04 -0300

    Cambio de urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
-from app.views import HomeView, ProtocolView, ActivityView, ProjectView, LocalExecutionView, FailureResolutionView
+from .views import HomeView, ProtocolView, ActivityView, ProjectView, LocalExecutionView, FailureResolutionView
+from .views import get_protocols_by_project
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("proyecto", ProjectView.as_view(), name="proyecto"),
     path("ejecucion_local", LocalExecutionView.as_view(), name="ejecucion_local"),
     path("resolucion_falla", FailureResolutionView.as_view(), name="resolucion_falla"),
+    path("get-protocols-by-project/", get_protocols_by_project, name="get_protocols_by_project"),
 ]
