8da5320e1cd402dbd6b8f14355f7cdc84c72bc9d
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-11-25 16:44:08 -0300

    Avances modulo notificaciones

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Activity, Protocol, ActivityProtocol, Project, ProtocolProject
+from .models import *
 
 admin.site.register(Activity)
 admin.site.register(Protocol)
 admin.site.register(ActivityProtocol)
 admin.site.register(Project)
 admin.site.register(ProtocolProject)
+admin.site.register(Notification)
