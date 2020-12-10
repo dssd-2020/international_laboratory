83a04edf2f0451b78d761dc419181d3149de3a1c
echeva <yaniiecheva@gmail.com>
2020-12-10 00:34:52 -0300

    Se agregan campos en los listados del admin

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-
 from .models import *
 
 admin.site.register(Activity)
-admin.site.register(Protocol)
-admin.site.register(ActivityProtocol)
-admin.site.register(Project)
-admin.site.register(ProtocolProject)
 admin.site.register(Notification)
+
+
+@admin.register(Protocol)
+class ProtocolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'order', 'is_local', 'points')
+
+
+@admin.register(ActivityProtocol)
+class ActivityProtocolAdmin(admin.ModelAdmin):
+    list_display = ('protocol', 'activity', 'approved')
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'project_manager', 'active', 'case_id', 'approved')
+
+
+@admin.register(ProtocolProject)
+class ProtocolProjectAdmin(admin.ModelAdmin):
+    list_display = ('protocol', 'project', 'result', 'approved', 'responsible', 'running_task')
